c707acfb172ad3553bd2056927ad813256db61f3
Roman <stud115405@vyatsu.ru>
2021-09-21 16:25:32 +0300

    2 commit

--- src/StringExtention.java
+++ src/StringExtention.java
@@ -4,15 +4,25 @@ public class StringExtention {
     public static boolean isIP(String str)
     {
         //Pattern pattern = Pattern.compile((str), Pattern.CASE_INSENSITIVE);
-        return str.matches("((\\d|[1-9]\\d|1\\d\\d|2[0-5]" +
+        return str.matches("^((\\d|[1-9]\\d|1\\d\\d|2[0-5]" +
                 "[0-5])\\.)((\\d|[1-9]\\d|1\\d\\d|2[0-5][0-5])" +
                 "\\.)((\\d|[1-9]\\d|1\\d\\d|2[0-5][0-5])\\.)" +
                 "((2[0-5][0-5])|[1-9]\\d|1\\d\\d|\\d)$");
     }//((\d|[1-9]\d|1\d\d|2[0-5][0-5])\.){3}\2
+
     public static boolean isGUID(String str)
     {
         //Pattern pattern = Pattern.compile((str), Pattern.CASE_INSENSITIVE);
-        return str.matches("[a-zA-Z0-9]{8}-[a-zA-Z0-9]{4}" +
-                "-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{12}");
+        return str.matches("^[a-zA-Z0-9]{8}-[a-zA-Z0-9]{4}" +
+                "-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{12}$");
     }//([a-zA-Z0-9]){8}-(\1{4})-\2-\2-\1{12}
+
+    public static boolean isURL(String str)
+    {
+        return str.matches("^https?:\\/\\/[^\\-](?:(?:([a-z0-9-]{1,128})" +
+                "?[^\\-]\\.)+(?:ru|su|com|net|org|edu)|(?!0)(?:(?!0[^.]|255)[0-9]" +
+                "{1,3}\\.){3}(?!0|255)[0-9]{1,3})(?:\\/[a-z0-9.,_@%&?+=\\~/-]*)" +
+                "?(?:#[^ '\\\"&]*)?$");
+    }
+    //
 }
